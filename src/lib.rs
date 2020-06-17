b7e3004daabc3f549e397f369d8ba3c83e6d1b7c
Philipp Schuster <philipp.schuster@t-systems.com>
2020-06-17 12:01:46 +0200

    Better code + comments

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,16 @@ use std::thread::sleep;
 use std::time::Duration;
 use std::io::{stdout, Write};
 
+const ANIMATION_STEPS_PER_SECOND: u64 = 4;
+const ANIMATION_SLEEP_TIMEOUT: u64 = 1000 / ANIMATION_STEPS_PER_SECOND;
+
 /// The chars for the animation (the loading spinner).
 static ANIMATION_STATES: [char; 4] = ['—', '\\', '|', '/'];
 
 /// Displays a loading animation on stdout. From and To are both values that are not necessarily
 /// percent. They describe to problem range, e.g. copy files 1000 to 2000.
 ///
-/// One need to provide a function that returns the progress in percent at any given time.
+/// One must provide a function that returns the progress in percent at any given time.
 pub fn show_loading_animation(from: usize,
                               to: usize,
                               progress_in_percentage_fn: &dyn Fn() -> usize) {
@@ -16,12 +19,17 @@ pub fn show_loading_animation(from: usize,
 
     let mut animation_step = 0;
     loop {
-        let curr_percent = progress_in_percentage_fn();
-        let curr_progress = calc_progress(from, to, curr_percent);
+        let mut curr_percent = progress_in_percentage_fn();
+        let mut curr_progress = calc_progress(from, to, curr_percent);
+
+        // be a little bit more failure tolerant
+        if curr_percent > 100 { curr_percent = 100; }
+        if curr_progress > to { curr_progress = to }
 
         // current char of the loading animation
         let indicator = format!("{}", ANIMATION_STATES[animation_step]);
 
+        // msg to be printed; \r is important to reset the current line
         let msg = format!("\r [{}] {}/{} ({}%)", indicator.as_str(), curr_progress, to, curr_percent);
 
         stdout().write(msg.as_bytes()).unwrap();
@@ -31,12 +39,14 @@ pub fn show_loading_animation(from: usize,
         // break when we're done
         if curr_progress >= to { break; }
 
+        // switch to next animation step / next char
         animation_step = (animation_step + 1) % ANIMATION_STATES.len();
-        // 4 changes/animation steps per second
-        sleep(Duration::from_millis(1000 / 4));
+
+        sleep(Duration::from_millis(ANIMATION_SLEEP_TIMEOUT));
     }
 }
 
+/// Calculates the progress from the given percentage.
 fn calc_progress(from: usize, to: usize, percent: usize) -> usize {
     let normalized = (to - from) as f64;
     let progress = (percent as f64) / 100_f64;
