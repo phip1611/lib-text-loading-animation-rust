15a7e71093d47e33a860375226285576a8c29f8b
Philipp Schuster <phip1611@gmail.com>
2020-06-19 15:28:31 +0200

    C example with worker thread

--- calling-from-c-examples/main.c
+++ calling-from-c-examples/main.c
@@ -1,14 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <unistd.h> // usleep
 
 typedef unsigned long long usize; // 64 bit
 extern void show_loading_animation_ffi(usize, usize, usize (*prog_fn)());
 
-usize progress_reporter() {
-    return (usize) 80;
+// shared global var
+usize counter;
+
+// function that tells the lib how many percent progress we made so far
+usize progress_reporter() { return counter; }
+
+// function where our worker thread makes progress
+void * progress_maker() {
+    while(counter < 100) {
+        counter++;
+        usleep(1000 * 50);
+    }
+    pthread_exit(NULL);
 }
 
 int main(void) {
+    pthread_t t1;
+    // start thread
+    pthread_create(&t1, NULL, progress_maker, NULL);
     show_loading_animation_ffi(0, 100, progress_reporter);
     return 0;
 }
