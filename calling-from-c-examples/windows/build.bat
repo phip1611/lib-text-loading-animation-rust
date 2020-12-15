3380290f867d36e8ba0040322da2f3bfb12664ae
Philipp Schuster <phip1611@gmail.com>
2020-12-15 11:48:21 +0100

    Building/Linking on Windows works again

--- calling-from-c-examples/windows/build.bat
+++ calling-from-c-examples/windows/build.bat
@@ -8,6 +8,11 @@ cd "calling-from-c-examples/windows"
 xcopy /y ..\..\target\release\text_loading_animation.dll .
 xcopy /y ..\..\target\release\text_loading_animation.dll.lib .
 
+set PROMPT_CONFIGURE_DIR="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+echo "###############################################################"
+echo "Make sure that Visual Studio Build tools are installed and that"
+echo "%PROMPT_CONFIGURE_DIR% exists!"
+echo "###############################################################"
 
 rem: sets up environment: it's a complicated process for Windows where a lot of env vars must be set
 rem: see: https://docs.microsoft.com/de-de/cpp/build/building-on-the-command-line?view=vs-2019
@@ -17,7 +22,7 @@ rem: doesn't work: header files are not found: call "C:\Program Files (x86)\Micr
 IF NOT DEFINED VCToolsInstallDir (
     rem: we just check if any of the vars that should be defined is defined; in this case we use "VCToolsInstallDir"
     rem: to prevent this to be run multiple time in the same terminal (otherwise PATH gets too long)
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+    call %PROMPT_CONFIGURE_DIR%
 )
 rem: *.dll.lib contains header information during compile; *.dll is the runtime file that must be in the same directory/path
 
